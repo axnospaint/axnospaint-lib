e6941bf1621ed60abd5fcb880be98ef78304dc60
axnospaint <122679113+axnospaint@users.noreply.github.com>
2026-03-04 04:42:03 +0900

    overrides: serialize-javascript": "7.0.3" (#98)
    npmパッケージのupdate
    json-loaderの削除（webpack5に内蔵のため不要）

--- webpack.config.js
+++ webpack.config.js
@@ -35,11 +35,6 @@ module.exports = {
         type: 'asset/source',
       },
       {
-        test: /\.json$/,
-        loader: 'json-loader',
-        type: 'javascript/auto',
-      },
-      {
         test: /\.png/,
         type: 'asset/inline',
       },
